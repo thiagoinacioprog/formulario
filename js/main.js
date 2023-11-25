51a01c60497413fb5c505adf8ae58c9b94fe6f34
Thiago Inacio de Oliveira <ART0018528@aluno.firjansenaisesi.com.br>
2023-11-24 21:50:34 -0300

    atualização formulario

---
+++ js/main.js
@@ -0,0 +1,25 @@
+ function save(){
+    var nome = 
+    document.getElementById("nome").value;
+ 
+    var sexo = 
+    document.getElementById("sexo").value;
+ 
+    var turma = 
+    document.getElementById("turma").value;
+ 
+    var av1 = 
+    document.getElementById("av1").value;
+ 
+    var av2 = 
+    document.getElementById("av2").value;
+
+    console.log(nome);
+   console.log(sexo);
+   console.log(turma);
+   console.log(av1);
+   console.log(av2);
+
+    }
+
+    
